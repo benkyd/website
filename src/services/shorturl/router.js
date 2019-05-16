a2f7440aa2ef06596385cd9de4db3948f3db18e5
Ben <playsplane@gmail.com>
2019-05-16 14:47:07 +0100

    URL Shortener service working

--- src/services/shorturl/router.js
+++ src/services/shorturl/router.js
@@ -17,9 +17,37 @@ router.get('/:endpoint', async (req, res, next) => {
     res.redirect(301, endpoint.target);
 });
 
-router.post('/', (req, res, next) => {
-    console.log(req.body);
-    res.send(req.body);
+/**
+ * TODO: Add caching of most frequently used short URLs, and to lower database volume check
+ * if a target URL already exists and then just give the user the endpoint of that one
+ * instead of making another pointless database entry
+ */
+router.post('/', async (req, res) => {
+    let target = req.body.url;
+
+    // Regex to validate URL
+    let reg = new RegExp(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/);
+
+    if (!reg.test(target)) {
+        res.send(JSON.stringify({ url: "Url Provided is not valid"}));
+        return;
+    }
+
+    // Lol this is probably so bad and will break on any edge cases
+    if (target.startsWith('www') || !target.startsWith('http')) {
+        target = 'http://' + target;
+    }
+
+    let endpoint = await Controller.genEndpoint();
+
+    let response = {
+        url: '/url/' + endpoint
+    };
+
+    res.send(response);
+
+    Database.newURL(endpoint, target);
+
 });
 
 module.exports = router;
