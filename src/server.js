4c3c9c4452e2466141c9a918b0bbdcddb2826bbf
Benjamin Kyd <benjaminkyd@gmail.com>
2019-05-26 01:14:27 +0100

    Changed express configuration

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const Logger = require('./logger.js');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
 module.exports.app;
 module.exports.server;
@@ -14,9 +15,9 @@ module.exports.init = async function init(port) {
     Logger.info('Server Settup');
 
     try {
-        module.exports.app.use(bodyParser.json());
-        module.exports.app.use(bodyParser.json({limit: "500mb"}));
-        module.exports.app.use(bodyParser.urlencoded({limit: "500mb", extended: true, parameterLimit:50000}));
+        module.exports.app.use(bodyParser.urlencoded({limit: "5000mb", extended: true, parameterLimit:50000}));
+        module.exports.app.use(bodyParser({limit: '5000mb'}));
+
         module.exports.app.listen(port);
     } catch (e) {
         Logger.panic(`Could not open a connection on port ${port}, maybe the port is populated or permissions are not met`);
--- src/services/schost/pagerouter.js
+++ src/services/schost/pagerouter.js
@@ -11,12 +11,7 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     let img = req.body.img;
-    let base64Data;
-    base64Data = img.replace(/^data:image\/png;base64,/,"");
-    base64Data = img.replace(/^data:image\/pjpeg;base64,/,"");
-    base64Data = img.replace(/^data:image\/jpg;base64,/,"");
-
-    base64Data  +=  base64Data.replace('+', ' ');
+    if (!img) return;
 
     let filepath = await base64Img.imgSync(img, './storage' /* path */, '2' /* name */);
     console.log(filepath)
