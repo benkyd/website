2d4d4b171d2477ee4a823abf9624ecc83dfd9960
Ben <benjaminkyd@gmail.com>
2021-09-27 20:27:17 +0100

    more proffessional ennit

--- client/index.js
+++ client/index.js
@@ -1,24 +1,12 @@
-// Opera 8.0+
-var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+$(document).ready(function() {
 
-// Firefox 1.0+
-var isFirefox = typeof InstallTrigger !== 'undefined';
-
-// Safari 3.0+ "[object HTMLElementConstructor]" 
-var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
-
-// Internet Explorer 6-11
-var isIE = /*@cc_on!@*/false || !!document.documentMode;
-
-// Edge 20+
-var isEdge = !isIE && !!window.StyleMedia;
-
-// Chrome 1 - 71
-var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
-
-// Blink engine detection
-var isBlink = (isChrome || isOpera) && !!window.CSS;
-
-if (isFirefox) {
-    alert('Alert! Firefox is trash and resizes SVGs wrong, please use a good browser :)');
-}
+    $('.link').on('click', function(e){
+      
+      var hash = $(this).attr('href');
+       $('html,body').animate({
+         scrollTop: $(hash).offset().top
+       }, 800);
+      
+      return false;
+    });
+});
