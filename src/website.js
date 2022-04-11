2a48c9353e2e2fe28e8212b34b5a3d25ac4be9d3
Ben <36240171+benkyd@users.noreply.github.com>
2022-04-11 18:53:06 +0100

    ok

--- src/website.js
+++ src/website.js
@@ -5,5 +5,12 @@ const express = require('express');
 
 module.exports.init = async function(path) {
     Server.app.use(express.static(path))
+
+    Server.app.get('/steam', (req, res, next) => {
+        // log ip of steam account accessor and redirect
+        Logger.info(`Steam account accessed from ${req.ip}`);
+        res.redirect('https://steamcommunity.com/id/plane000/');
+    });
+
     Logger.info('Website being staticaly served to root');
 }
